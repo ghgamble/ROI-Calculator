746431cd2aa2c429d8a4d4ac644cd21e3413580d
Grace Gamble <grace.h.gamble@gmail.com>
2016-08-27 18:55:50 -0600

    final updates to project

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ angular.module("roiCalculator", [])
 
      	cCtrl.totalOneTimeRevenue = 0;
      	cCtrl.totalMonthlyRevenue = 0;
+          cCtrl.totalRevenue  = 0;
           cCtrl.totalOneTimeExpense = 0;
           cCtrl.totalMonthlyExpense = 0;
-     	cCtrl.totalRevenue  = 0;
           cCtrl.totalExpenses = 0;
           cCtrl.monthlyContributionProfit = 0;
           cCtrl.totalContributionProfit = 0;
@@ -19,13 +19,13 @@ angular.module("roiCalculator", [])
           cCtrl.capitalRoi = 0;
 
           cCtrl.deleteRevenue = function($index) {
-               cCtrl.oneTimeRevenue -= cCtrl.revenueItems[$index].oneTimeRevenue;
-               cCtrl.monthlyRevenue -= cCtrl.revenueItems[$index].monthlyRevenue;
+               cCtrl.totalOneTimeRevenue -= cCtrl.revenueItems[$index].oneTimeRevenue;
+               cCtrl.totalMonthlyRevenue -= cCtrl.revenueItems[$index].monthlyRevenue;
                cCtrl.totalRevenue -= (cCtrl.revenueItems[$index].oneTimeRevenue + (cCtrl.revenueItems[$index].monthlyRevenue * 12));
                cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpenses;
                cCtrl.totalContributionProfit = cCtrl.totalRevenue - cCtrl.totalExpenses;
                cCtrl.contributionMargin = cCtrl.totalContributionProfit/cCtrl.totalRevenue;
-               cCtrl.capitalRoi = (cCtrl.oneTimeExpense - cCtrl.oneTimeRevenue)/cCtrl.monthlyContributionProfit;
+               cCtrl.capitalRoi = (cCtrl.totalOneTimeExpense - cCtrl.totalOneTimeRevenue)/cCtrl.monthlyContributionProfit;
                cCtrl.revenueItems.splice($index, 1);
           };
           cCtrl.addRevenue = function(){
@@ -35,7 +35,7 @@ angular.module("roiCalculator", [])
           	cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpenses;
           	cCtrl.totalContributionProfit = cCtrl.totalRevenue - cCtrl.totalExpenses;
           	cCtrl.contributionMargin = cCtrl.totalContributionProfit/cCtrl.totalRevenue;
-          	cCtrl.capitalRoi = (cCtrl.oneTimeExpense - cCtrl.oneTimeRevenue)/cCtrl.monthlyContributionProfit;
+          	cCtrl.capitalRoi = (cCtrl.totalOneTimeExpense - cCtrl.totalOneTimeRevenue)/cCtrl.monthlyContributionProfit;
           	cCtrl.revenueItems.push({
           		revenueName: cCtrl.revenueName||"noName",
           		oneTimeRevenue: cCtrl.oneTimeRevenue||0,
@@ -46,31 +46,31 @@ angular.module("roiCalculator", [])
           	cCtrl.monthlyRevenue = null;
           };
           cCtrl.deleteExpense = function($index){
-               cCtrl.oneTimeExpense -= cCtrl.expenseItems[$index].oneTimeExpense;
-               cCtrl.monthlyExpense -= (cCtrl.expenseItems[$index].monthlyExpense);
-               cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpenses;
+               cCtrl.totalOneTimeExpense -= cCtrl.expenseItems[$index].oneTimeExpense;
+               cCtrl.totalMonthlyExpense -= (cCtrl.expenseItems[$index].monthlyExpense);
+               cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpense;
                cCtrl.totalExpenses -= (cCtrl.expenseItems[$index].oneTimeExpense + (cCtrl.expenseItems[$index].monthlyExpense * 12));
                cCtrl.totalContributionProfit = cCtrl.totalExpenses - cCtrl.totalRevenue;
                cCtrl.contributionMargin = cCtrl.totalContributionProfit/cCtrl.totalRevenue;
-               cCtrl.capitalRoi = (cCtrl.oneTimeExpense - cCtrl.oneTimeRevenue)/cCtrl.monthlyContributionProfit;
+               cCtrl.capitalRoi = (cCtrl.totalOneTimeExpense - cCtrl.totalOneTimeRevenue)/cCtrl.monthlyContributionProfit;
                cCtrl.expenseItems.splice($index, 1);
           };
           cCtrl.addExpense = function(){
-               cCtrl.totalOneTimeExpense += cCtrl.oneTimeExpense||0;
-               cCtrl.totalMonthlyExpense += cCtrl.monthlyExpense||0;
-               cCtrl.totalExpenses += (cCtrl.oneTimeExpense + (cCtrl.monthlyExpense * 12));
-               cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpenses;
-               cCtrl.totalContributionProfit = cCtrl.totalRevenue - cCtrl.totalExpenses;
-               cCtrl.contributionMargin = cCtrl.totalContributionProfit/cCtrl.totalRevenue;
-               cCtrl.capitalRoi = (cCtrl.oneTimeExpense - cCtrl.oneTimeRevenue)/cCtrl.monthlyContributionProfit;
-               cCtrl.expenseItems.push({
-                    expenseName: cCtrl.expenseName||"noName",
-                    oneTimeExpense: cCtrl.oneTimeExpense||0,
-                    monthlyExpense: cCtrl.monthlyExpense||0
-               });
-               cCtrl.expenseName = "";
-               cCtrl.oneTimeExpense = null;
-               cCtrl.monthlyRevenue = null;
+          	cCtrl.totalOneTimeExpense += cCtrl.oneTimeExpense||0;
+          	cCtrl.totalMonthlyExpense += cCtrl.monthlyExpense||0;
+          	cCtrl.totalExpenses += (cCtrl.oneTimeExpense + (cCtrl.monthlyExpense * 12));
+          	cCtrl.monthlyContributionProfit = cCtrl.totalMonthlyRevenue - cCtrl.totalMonthlyExpense;
+          	cCtrl.totalContributionProfit = cCtrl.totalRevenue - cCtrl.totalExpenses;
+          	cCtrl.contributionMargin = cCtrl.totalContributionProfit/cCtrl.totalRevenue;
+          	cCtrl.capitalRoi = (cCtrl.totalOneTimeExpense - cCtrl.totalOneTimeRevenue)/cCtrl.monthlyContributionProfit;
+          	cCtrl.expenseItems.push({
+          		expenseName: cCtrl.expenseName||"noName",
+          		oneTimeExpense: cCtrl.oneTimeExpense||0,
+          		monthlyExpense: cCtrl.monthlyExpense||0
+          	});
+          	cCtrl.expenseName = "";
+          	cCtrl.oneTimeExpense = null;
+          	cCtrl.monthlyExpense = null;
           };
      };
 })();
